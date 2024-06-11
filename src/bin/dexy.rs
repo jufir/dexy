aa6968211969ba72c9bd520276c5fa163da13d1c
glomdom <22115@uktc-bg.com>
2024-06-11 16:23:46 +0300

    feat: add `clap`

--- src/bin/dexy.rs
+++ src/bin/dexy.rs
@@ -1,12 +1,19 @@
 use std::fs::File;
-
+use clap::Parser;
 use dexy::Dex;
 
+#[derive(clap::Parser)]
+struct Cli {
+    /// DEX file to use
+    file: std::path::PathBuf
+}
+
 fn main() -> anyhow::Result<()> {
-    let mut file = File::open("classes.dex")?;
+    let args = Cli::parse();
+
+    let mut file = File::open(args.file)?;
     let dex = Dex::parse(&mut file)?;
 
     println!("{:?}", dex);
-
     Ok(())
 }
